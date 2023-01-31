2788ffef32f5edb5c638baf1d504d972aa99e9f5
Alan Feng <alanfeng_2020@qq.com>
2023-01-31 18:00:23 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,9 +31,7 @@ black_list = '''
 pass_list = '''
 '''
 
-HOST = '127.0.0.1'  # 监听地址，建议监听本地然后由web服务器反代
-PORT = 80  # 监听端口
-ASSET_URL = 'https://hunshcn.github.io/gh-proxy'  # 主页
+ASSET_URL = 'https://ffha.github.io/gh-proxy'  # 主页
 
 white_list = [tuple([x.replace(' ', '') for x in i.split('/')]) for i in white_list.split('\n') if i]
 black_list = [tuple([x.replace(' ', '') for x in i.split('/')]) for i in black_list.split('\n') if i]
@@ -187,7 +185,3 @@ def proxy(u, allow_redirects=False):
     except Exception as e:
         headers['content-type'] = 'text/html; charset=UTF-8'
         return Response('server error ' + str(e), status=500, headers=headers)
-
-app.debug = True
-if __name__ == '__main__':
-    app.run(host=HOST, port=PORT)
